c2722d00e1d4622335bbf0603951e5e807648847
simon <simon.orchard1@btinternet.com>
2024-12-24 21:49:34 +0000

    Add part 2 solution.

--- 2024_12_01/src/main.rs
+++ 2024_12_01/src/main.rs
@@ -1,6 +1,6 @@
 
 use std::fs;
-
+use std::collections::HashMap;
 
 fn main() -> std::io::Result<()> {
     // Load the file
@@ -22,6 +22,15 @@ fn main() -> std::io::Result<()> {
         v_right.push(z.parse().unwrap());
     }
 
+    let mut v_left_original: Vec<i32> = Vec::new();
+    let mut v_right_original: Vec<i32> = Vec::new();
+    for (i, el) in v_left.iter().enumerate() {
+        v_left_original.push(*el);
+    }
+    for (i, el) in v_right.iter().enumerate() {
+        v_right_original.push(*el);
+    }
+
     // Sort both vectors
     v_left.sort();
     v_right.sort();
@@ -37,7 +46,28 @@ fn main() -> std::io::Result<()> {
     for i in &v_diff {
         total += i;
     }
-    println!("{total}");
+    println!("Part 1 is {total}");
 
+    let mut m: HashMap<i32, usize> = HashMap::new();
+    for x in v_right_original {
+        *m.entry(x).or_default() += 1;
+    }
+    let mut part_2: i32 = 0;
+    for x in v_left_original {
+        let t:i32;
+        let ss:i32;
+
+        if (m.contains_key(&x)) == true {
+            t = m[&x] as i32;
+        }
+        else {
+            t = 0;
+        }
+        // println!("{t}");
+        ss = t*x;
+
+        part_2 +=ss;
+    }
+    println!("Part 2 is {part_2}");
     Ok(())
 }
